72f0bab11059abe1deb350e6c9b6183722549080
LeonardoAraujoDemicio <69490203+LeonardoAraujoDemicio@users.noreply.github.com>
2020-10-30 19:39:44 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Exercício_encapsulamento_Leo_A._Demicio
+{
+    public class Produto
+    {
+        public string Nome
+        {
+            get { return Nome; }
+
+            set { if (value != null)
+                {
+                    Nome = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get {return Preco; }
+        }
+
+        public int Quantidade
+        {
+            get { return Quantidade; }
+        }
+
+        public Produto  (string nome, double preco, int quantidade)
+        {
+            nome = Nome;
+            preco = Preco;
+            quantidade = Quantidade;
+        }
+
+       
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercício_Construtores_Leo_A._Demicio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // criação de um novo objeto.
+            Produto p = new Produto();
+
+            // declaração das variáveis do objeto, incluindo nome, preço e quantidade.
+            Console.WriteLine("Entre com os dados do produto: "); 
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            // exibição dos dados referentes a declaração das variáveis.
+            Console.WriteLine("Dados do produto: " + p);
+
+            // adição de X produtos ao estoque.
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+            
+            // exibição dos dadeos atualizados.
+            Console.WriteLine("Dados atualizados: " + p);
+
+            // remoção de X produtos do estoque.
+            Console.WriteLine("Digite o número de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte);
+
+            // exibição final dos dados.
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+         
